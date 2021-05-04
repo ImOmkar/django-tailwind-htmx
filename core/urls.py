99c2c779e309778e9fcb57ff491e3232c04df01e
Omkar Parab <42148962+ImOmkar@users.noreply.github.com>
2021-05-04 12:49:08 +0530

    lazy loading to task_list on home page

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, delete_task, create_task, update_task
+from .views import home, delete_task, create_task, update_task, task_list
 
 urlpatterns = [
     path('', home, name="home"),
+    path('tasks/', task_list, name="task_list"),
     path('tasks/create/', create_task, name="create_task"),
     path('tasks/<int:id>/update/', update_task, name="update_task"),
     path('tasks/<int:id>/delete/', delete_task, name="delete_task"),
